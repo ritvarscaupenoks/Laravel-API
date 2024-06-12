73ac77848022aa9bd618919529bbb97267daecff
Ritvars Caupenoks <ritvarscaupenoks@gmail.com>
2024-06-12 17:56:35 +0300

    Add movies table migration

---
+++ database/migrations/2024_06_12_144611_create_movies_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->float('rating');
+            $table->char('age_restriction', 2)->nullable();
+            $table->string('description', 500);
+            $table->timestamp('premieres_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movies');
+    }
+};
