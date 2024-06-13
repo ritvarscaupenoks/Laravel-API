249b0847174cebf9c733a2e9dbba100fcdbbf6f4
Ritvars Caupenoks <ritvarscaupenoks@gmail.com>
2024-06-13 14:55:00 +0300

    Add model event to prevent duplicate movies

--- app/Models/MovieBroadcast.php
+++ app/Models/MovieBroadcast.php
@@ -19,4 +19,21 @@ class MovieBroadcast extends Model
     {
         return $this->belongsTo(Movie::class);
     }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+
+            $exists = MovieBroadcast::where('movie_id', $model->movie_id)
+                ->where('channel_nr', $model->channel_nr)
+                ->whereDate('broadcasts_at', \Carbon\Carbon::parse($model->broadcasts_at)->toDateString())
+                ->exists();
+
+            if ($exists) {
+                throw new \Exception('The movie is already scheduled on this channel for this day.');
+            }
+        });
+    }
 }
